3d83d672e38918f25c249acc256c24a24c7e613f
gordao-dev <contato.gmds@gmail.com>
2022-10-25 12:06:41 -0300

    deleted

--- prisma/migrations/20221021185817_inscrever_no_pedal/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "InscreverNoPedal" (
-    "ride_id" SERIAL NOT NULL,
-    "user_id" INTEGER NOT NULL,
-    "subscription_date" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "InscreverNoPedal_pkey" PRIMARY KEY ("ride_id")
-);
--- src/Modules/Pedaladas/Repositories/ICreatePedaladasRepository.ts
+++
@@ -1,9 +0,0 @@
-import CriarPedaladasInput from "../../../dtos/inputs/create-pedaladas-input";
-import CriarPedaladaInput from "../DTOs/inputs/CreatePedaladasInput";
-import CriarPedaladasModel from "../DTOs/models/PedaladasModels";
-
-interface IPedaladasRepository {
-  create(data: CriarPedaladaInput): Promise<CriarPedaladasModel>;
-}
-
-export default IPedaladasRepository;
